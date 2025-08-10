335febb6d7dd348a28c4bc1083de42733955b960
Bennette <156165975+Bennette321321@users.noreply.github.com>
2025-08-10 14:18:56 +0700

    Test API (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  // const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:5000/api/hello")
-  //     .then((res) => setMessage(res.data.message))
-  //     .catch((err) => console.error(err));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/hello")
+      .then((res) => setMessage(res.data.message))
+      .catch((err) => console.error(err));
+  }, []);
 
-  return <h1 className="text-2xl text-red-500">Hello World</h1>
-
-  // return (
-  //   <main className="text-2xl p-4">
-  //     <h1 className="font-bold">Frontend is Working!</h1>
-  //     <p className="font-semibold">Backend says: {message || "Loading..."}</p>
-  //   </main>
-  // );
+  return (
+    <main className="text-2xl p-4">
+      <h1 className="font-bold">Frontend is Working!</h1>
+      <p className="font-semibold text-red-500">Backend says: {message || "Loading..."}</p>
+    </main>
+  );
 }
 
 export default App;
